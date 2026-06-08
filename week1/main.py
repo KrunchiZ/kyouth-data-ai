4a4d5d61e1ac850b7c83773dd57ddc90c5f7e135
kchiang <kaizhi3006@gmail.com>
2026-06-08 18:48:51 +0800

    feat(week1): Changed the code in main function in main.py to run modules 	based on command provided as argv to a match case logic.

--- week1/main.py
+++ week1/main.py
@@ -3,6 +3,7 @@ from src.ingestor import ingest_all_mhtml
 from src.processor import process_all_html
 from src.loader import load_all_jsons
 from src.profiler import run_data_profile
+import sys
 
 SOURCE_DIR = Path("data/0_source")
 BRONZE_DIR = Path("data/1_bronze")
@@ -11,29 +12,36 @@ GOLD_DIR = Path("data/3_gold")
 DB_NAME = "jobs.db"
 
 def run_profiler():
+    print("📊 Profiling: ...")
     run_data_profile(GOLD_DIR / DB_NAME)
 
 def run_gold():
+    print("🥇 Gold: ...")
     load_all_jsons(SILVER_DIR, GOLD_DIR)
 
 def run_silver():
+    print("🥈 Silver: ...")
     process_all_html(BRONZE_DIR, SILVER_DIR)
 
 def run_bronze():
+    print("🥉 Bronze: ...")
     ingest_all_mhtml(SOURCE_DIR, BRONZE_DIR)
 
 def main():
-    print("Starting ETL pipeline...")
-    stages = [
-        ("[1/4] Bronze: ingesting source files...",          run_bronze),
-        ("[2/4] Silver: cleaning and processing HTML...",    run_silver),
-        ("[3/4] Gold: loading structured data...",           run_gold),
-        ("[4/4] Profiling final database...",                run_profiler),
-    ]
-    for message, stage in stages:
-        print(message)
-        stage()
-    print("Pipeline complete.")
-
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <command>")
+        return
+    match sys.argv[1]:
+        case "ingest":
+            run_bronze()
+        case "process":
+            run_silver()
+        case "load":
+            run_gold()
+        case "profile":
+            run_profiler()
+        case _: # default case
+            print(f"Unknown command: {sys.argv[1]}")
+            
 if __name__ == "__main__":
     main()
