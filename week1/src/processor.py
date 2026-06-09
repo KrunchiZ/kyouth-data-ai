68705b696e7087d5c271950e891c19e30b26ab8d
kchiang <kaizhi3006@gmail.com>
2026-06-10 00:49:17 +0800

    feat(week1):
    Completed processor.py. Weird glitch in vscode when running the script
    where there is a missing space in Missing errors but appears fine on
    external terminal. Trivial but worth looking into.

--- week1/src/processor.py
+++ week1/src/processor.py
@@ -1,11 +1,5 @@
-import logging
-import json
-from pathlib import Path
 from bs4 import BeautifulSoup
-from pydantic import BaseModel, ValidationError
-
-
-logging.basicConfig(level=logging.INFO, format="%(message)s")
+from pydantic import BaseModel
 
 class JobListing(BaseModel):
     source_id: str
@@ -15,26 +9,73 @@ class JobListing(BaseModel):
 
 
 def process_all_html(input_dir, output_dir):
-    # Process all HTML files in the input_dir
-    # Strip HTML tags, derive source_id from metadata, clean the text
-    # Validate the cleaned text with pydantic
-    # Save the results to output_dir in .json format
-
     if not input_dir.exists():
-        logging.warning(f"⚠️ Input directory not found: {input_dir}")
+        print(f"⚠️ Input directory not found: {input_dir}")
         return
 
     process_count = 0
     skip_count = 0    
     output_dir.mkdir(parents=True, exist_ok=True)
     for html_file in input_dir.glob("*.html"):
-        with open(html_file, "r", encoding="utf-8") as in_file:
-            soup = BeautifulSoup(in_file, "html.parser")
-            source_id = (
-                soup.find("meta", property="og:url")["content"]
-                .rstrip("/").split("/")[-1]
-            )
-    
+        try:
+            with open(html_file, "r", encoding="utf-8") as in_file:
+                soup = BeautifulSoup(in_file, "html.parser")
+                source_id = (
+                    soup.find("meta", property="og:url")["content"]
+                    .rstrip("/").split("/")[-1]
+                )
+                job_title = get_soup_text(soup, "job-detail-title")
+                if not job_title:
+                    print(f"⚠️ Missing job title in: {html_file.name}")
+                    skip_count += 1
+                    continue
+                company = get_soup_text(soup, "advertiser-name")
+                if not company:
+                    print(f"⚠️ Missing company in: {html_file.name}")
+                    skip_count += 1
+                    continue
+                description = get_soup_text(soup, "jobAdDetails")
+                if not description:
+                    print(f"⚠️ Missing description in: {html_file.name}")
+                    skip_count += 1
+                    continue
+        except Exception as code:
+            print(f"⚠️ Error processing {html_file.name}: {code}")
+            skip_count += 1
+            continue
+
+        output_data = JobListing(
+            source_id = source_id,
+            job_title = job_title,
+            company = company,
+            description = description
+        )
+        if generate_json_success(output_dir, html_file, output_data):
+            process_count += 1
+        else:
+            skip_count += 1
+
     total_count = len(list(input_dir.glob("*.html")))
-    logging.info(f"\n📊 Silver Summary:\nTotal: {total_count} "
-                 f"| Processed: {process_count} | Skipped: {skip_count}")
+    print(f"\n📊 Silver Summary:\nTotal: {total_count} | "
+          f"Processed: {process_count} | Skipped: {skip_count}")
+
+
+def get_soup_text(soup, attr_value):    
+    tag = soup.find(attrs={"data-automation": attr_value})
+    if tag is None:
+        return None
+    if attr_value == "jobAdDetails":
+        return " ".join(tag.get_text(separator=" ", strip=True).split())
+    return tag.get_text(separator=" ", strip=True)
+
+
+def generate_json_success(output_dir, html_file, output_data):    
+    output_path = output_dir / (html_file.stem + ".json")
+    try:
+        with open(output_path, "w", encoding="utf-8") as out_file:
+            out_file.write(output_data.model_dump_json(indent=2))
+            print(f"✅ Processed: {html_file.name}")
+            return True
+    except Exception as code:
+        print(f"⚠️ Error writing JSON for {html_file.name}: {code}")
+        return False
