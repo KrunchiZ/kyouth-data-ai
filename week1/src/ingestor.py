05a5b435168e466caa33797f045e8dcd28043f3e
kchiang <kaizhi3006@gmail.com>
2026-06-10 00:19:50 +0800

    feat(week1): added more exception catch in ingestion.py when opening and writing input/output

--- week1/src/ingestor.py
+++ week1/src/ingestor.py
@@ -3,7 +3,6 @@ import quopri
 
 
 def ingest_all_mhtml(input_dir, output_dir):
-    
     if not input_dir.exists():
         print(f"⚠️ Input directory not found: {input_dir}")
         return
@@ -23,31 +22,40 @@ def ingest_all_mhtml(input_dir, output_dir):
 
 
 def ingest_mhtml(extract_count, failed_count, mhtml_file, output_dir):
-    
-    with open(mhtml_file, "rb") as in_file:
-        html_found = False
-        msg = email.message_from_binary_file(in_file)
-        for part in msg.walk():    
-            if part.get_content_type() == "text/html":
-                html_found = True
-                raw = part.get_payload()
-                if isinstance(raw, bytes):
-                    html_str = raw.decode("utf-8", errors="replace")
-                else:
-                    html_str = (
-                        quopri.decodestring(raw.encode()).decode("utf-8"
-                        , errors="replace")
-                    )
+    try:
+        with open(mhtml_file, "rb") as in_file:
+            html_found = False
+            msg = email.message_from_binary_file(in_file)
+            for part in msg.walk():    
+                if part.get_content_type() == "text/html":
+                    html_found = True
+                    raw = part.get_payload()
+                    if isinstance(raw, bytes):
+                        html_str = raw.decode("utf-8", errors="replace")
+                    else:
+                        html_str = (
+                            quopri.decodestring(raw.encode()).decode("utf-8"
+                            , errors="replace")
+                        )
 
-                output_path = output_dir / (mhtml_file.stem + ".html")
-                with open(output_path, "w", encoding="utf-8") as out_file:
-                    out_file.write(html_str)
-                    print(f"✅ Extracted: {mhtml_file.name}")
-                    extract_count += 1
-                    break
+                    output_path = output_dir / (mhtml_file.stem + ".html")
+                    try:
+                        with open(output_path, "w", encoding="utf-8") as out_file:
+                            out_file.write(html_str)
+                            print(f"✅ Extracted: {mhtml_file.name}")
+                            extract_count += 1
+                            break
+                    except Exception as code:
+                        print(f"⚠️️ Error writing {mhtml_file.name}: {code}")
+                        failed_count += 1
+                        break
 
-        if not html_found:
-            print(f"⚠️ No HTML content found in: {mhtml_file.name}")
-            failed_count += 1
+            if not html_found:
+                print(f"⚠️ No HTML content found in: {mhtml_file.name}")
+                failed_count += 1
+
+    except Exception as code:
+        print(f"⚠️ Error ingesting {mhtml_file.name}: {code}")
+        failed_count += 1
 
     return extract_count, failed_count
