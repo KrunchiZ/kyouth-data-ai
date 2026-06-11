20731a816953a4a4bd257bef26cd13bdc2a9d28a
kchiang <kaizhi3006@gmail.com>
2026-06-11 11:55:07 +0800

    chore: separated delete low quality profile into its own sql file

--- queries/quarantine_profiles.sql
+++ queries/quarantine_profiles.sql
@@ -1,10 +1,7 @@
 CREATE TABLE IF NOT EXISTS jobs_quarantine AS
 SELECT * FROM jobs
-WHERE 1=0;
+WHERE 0=0;
 
-INSERT OR IGNORE INTO jobs_quarantine
+INSERT OR REPLACE INTO jobs_quarantine
 SELECT * FROM jobs
-WHERE quality = 'LOW';
-
-DELETE FROM jobs
 WHERE quality = 'LOW';
