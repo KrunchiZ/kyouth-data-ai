512e90be72db30908004dd5817001be60f30ebca
kchiang <kaizhi3006@gmail.com>
2026-06-11 12:30:23 +0800

    fix: Rectify logic error in quarantine table creation where it wrongly copies everything over

--- queries/quarantine_profiles.sql
+++ queries/quarantine_profiles.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS jobs_quarantine AS
 SELECT * FROM jobs
-WHERE 0=0;
+WHERE 1=0;
 
 INSERT OR REPLACE INTO jobs_quarantine
 SELECT * FROM jobs
-WHERE quality = 'LOW';
+WHERE quality = "LOW";
