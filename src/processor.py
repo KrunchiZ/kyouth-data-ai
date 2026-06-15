0d8683d0ca07b2e0d42a24970d0444df70a74816
kchiang <kaizhi3006@gmail.com>
2026-06-15 12:24:17 +0800

    refactor: Using Validation Error and Field from pydantic for good.

--- src/processor.py
+++ src/processor.py
@@ -1,7 +1,8 @@
+from dataclasses import Field
 import os
 import logging
 from bs4 import BeautifulSoup
-from pydantic import BaseModel
+from pydantic import BaseModel, ValidationError, Field
 
 logging.basicConfig(
     level=logging.INFO,
@@ -9,10 +10,10 @@ logging.basicConfig(
 )
 
 class JobListing(BaseModel):
-    source_id: str
-    job_title: str
-    company: str
-    description: str
+    source_id   : str = Field(min_length=1)
+    job_title   : str = Field(min_length=1)
+    company     : str = Field(min_length=1)
+    description : str = Field(min_length=1)
 
 
 def process_all_html(input_dir, output_dir):
@@ -29,29 +30,31 @@ def process_all_html(input_dir, output_dir):
                 source_id = (soup.find("meta", property="og:url")["content"]
                             .rstrip("/").split("/")[-1])
                 job_title = get_soup_text(soup, "job-detail-title")
-                if not job_title:
-                    logging.warning(f"Missing job title in: {html_file.name}")
-                    continue
                 company = get_soup_text(soup, "advertiser-name")
-                if not company:
-                    logging.warning(f"Missing company in: {html_file.name}")
-                    continue
                 description = get_soup_text(soup, "jobAdDetails")
-                if not description:
-                    logging.warning(f"Missing description in: {html_file.name}")
-                    continue
         except Exception as code:
             logging.error(f"Error processing {html_file.name}: {code}")
             continue
 
-        output_data = JobListing(
-            source_id = source_id,
-            job_title = job_title,
-            company = company,
-            description = description
-        )
-        if generate_json_success(output_dir, html_file, output_data):
-            process_count += 1
+        try:
+            output_data = JobListing(
+                source_id = source_id,
+                job_title = job_title,
+                company = company,
+                description = description
+            )
+            output_path = output_dir / (html_file.stem + ".json")
+            with open(output_path, "w", encoding="utf-8") as out_file:
+                out_file.write(output_data.model_dump_json(indent=2))
+                logging.info(f"Processed: {html_file.name}")
+                process_count += 1
+
+        except ValidationError as code:
+            for error in code.errors():
+                logging.error(f"Missing {error['loc'][0].strip()} "
+                              f"in {html_file.name}")
+        except Exception as code:
+            logging.error(f"{code}: {html_file.name}")
 
     total_count = len(list(input_dir.glob("*.html")))
     print(f"\n📊 Silver Summary:\nTotal: {total_count} | Processed: "
@@ -84,15 +87,3 @@ def get_soup_text(soup, attr_value):
     if attr_value == "jobAdDetails":
         return " ".join(tag.get_text(separator=" ", strip=True).split())
     return tag.get_text(separator=" ", strip=True)
-
-
-def generate_json_success(output_dir, html_file, output_data):    
-    output_path = output_dir / (html_file.stem + ".json")
-    try:
-        with open(output_path, "w", encoding="utf-8") as out_file:
-            out_file.write(output_data.model_dump_json(indent=2))
-            logging.info(f"Processed: {html_file.name}")
-            return True
-    except Exception as code:
-        logging.error(f"Error writing JSON for {html_file.name}: {code}")
-        return False
